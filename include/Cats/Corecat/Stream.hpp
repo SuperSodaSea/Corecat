d34d6e915115080d80bc9ab0300680e55f1db4b9
SuperSodaSea <bobby825@126.com>
2017-01-08 21:30:58 +0800

    Add MemoryStream

--- include/Cats/Corecat/Stream.hpp
+++ include/Cats/Corecat/Stream.hpp
@@ -50,14 +50,14 @@ struct Stream {
     enum class SeekOrigin { Begin, Current, End };
     
     virtual bool isReadable() const { return false; };
-    virtual T read() { T data; return read(&data, 1) ? data : 0; }
-    virtual std::size_t read(T* /*data*/, std::size_t /*count*/) { throw std::runtime_error("stream is not readable"); }
-    virtual T peek() { T data; return peek(&data, 1) ? data : 0; }
-    virtual std::size_t peek(T* /*data*/, std::size_t /*count*/) { throw std::runtime_error("stream is not readable"); }
+    virtual T read() { T value; return read(&value, 1) ? value : 0; }
+    virtual std::size_t read(T* /*buffer*/, std::size_t /*count*/) { throw std::runtime_error("stream is not readable"); }
+    virtual T peek() { T value; return peek(&value, 1) ? value : 0; }
+    virtual std::size_t peek(T* /*buffer*/, std::size_t /*count*/) { throw std::runtime_error("stream is not readable"); }
     
     virtual bool isWriteable() const { return false; };
-    virtual void write(T data) { write(&data, 1); }
-    virtual void write(const T* /*data*/, std::size_t /*count*/) { throw std::runtime_error("stream is not writeable"); }
+    virtual void write(T value) { write(&value, 1); }
+    virtual void write(const T* /*buffer*/, std::size_t /*count*/) { throw std::runtime_error("stream is not writeable"); }
     virtual void flush() { throw std::runtime_error("stream is not writeable"); }
     
     virtual bool isSeekable() const { return false; };
@@ -108,9 +108,9 @@ public:
         
     }
     
-    bool isReadable() const override { return stream->isReadable(); };
+    bool isReadable() const override { return stream->isReadable(); }
     using Stream<T>::read;
-    std::size_t read(T* data, std::size_t count) override {
+    std::size_t read(T* buffer, std::size_t count) override {
         
         if(!readBufferData) {
             
@@ -120,14 +120,14 @@ public:
         }
         if(readBufferStart == readBufferData) readBufferEnd += stream->read(readBufferEnd, readBufferData + bufferSize - readBufferEnd);
         std::size_t res = std::min(count, static_cast<std::size_t>(readBufferEnd - readBufferStart));
-        std::copy(readBufferStart, readBufferStart + res, data);
+        std::copy(readBufferStart, readBufferStart + res, buffer);
         readBufferStart += res;
         if(readBufferStart == readBufferEnd) readBufferStart = readBufferEnd = readBufferData;
         return res;
         
     }
     using Stream<T>::peek;
-    std::size_t peek(T* data, std::size_t count) override {
+    std::size_t peek(T* buffer, std::size_t count) override {
         
         if(!readBufferData) {
             
@@ -137,23 +137,23 @@ public:
         }
         if(static_cast<std::size_t>(readBufferEnd - readBufferStart) >= count) {
             
-            std::copy(readBufferStart, readBufferStart + count, data);
+            std::copy(readBufferStart, readBufferStart + count, buffer);
             return count;
             
         } else {
             
             readBufferEnd += stream->read(readBufferEnd, readBufferData + bufferSize - readBufferEnd);
             std::size_t res = std::min(count, static_cast<std::size_t>(readBufferEnd - readBufferStart));
-            std::copy(readBufferStart, readBufferStart + res, data);
+            std::copy(readBufferStart, readBufferStart + res, buffer);
             return res;
             
         }
         
     }
     
-    bool isWriteable() const override { return stream->isWriteable(); };
+    bool isWriteable() const override { return stream->isWriteable(); }
     using Stream<T>::write;
-    void write(const T* data, std::size_t count) override {
+    void write(const T* buffer, std::size_t count) override {
         
         if(!writeBufferData) {
             
@@ -164,10 +164,10 @@ public:
         if(writeBufferEnd - writeBufferData + count > bufferSize) {
             
             flush();
-            if(count >= bufferSize) { stream->write(data, count); return; }
+            if(count >= bufferSize) { stream->write(buffer, count); return; }
             
         }
-        writeBufferEnd = std::copy(data, data + count, writeBufferEnd);
+        writeBufferEnd = std::copy(buffer, buffer + count, writeBufferEnd);
         
     }
     void flush() override {
@@ -177,8 +177,12 @@ public:
         
     }
     
-    bool isSeekable() const override { return stream->isSeekable(); };
+    bool isSeekable() const override { return stream->isSeekable(); }
+    using typename Stream<T>::SeekOrigin;
     std::intmax_t seek(std::intmax_t offset, SeekOrigin origin) override {
+        
+        return position;
+        
     }
     std::intmax_t getPosition() override {
         
@@ -201,6 +205,90 @@ template <typename T>
 inline BufferedStream<T> createBufferedStream(Stream<T>& t) { return BufferedStream<T>(t); }
 
 
+template <typename T>
+class MemoryStream : public Stream<T> {
+    
+private:
+    
+    T* data;
+    T* end;
+    T* cur;
+    
+public:
+    
+    MemoryStream(T* data_, std::size_t size) : data(data_), end(data + size), cur(data) {}
+    
+    bool isReadable() const override { return true; }
+    T read() override {
+        
+        if(cur == end) return 0;
+        else return *(cur++);
+        
+    }
+    std::size_t read(T* buffer, std::size_t count) override {
+        
+        std::size_t res = std::min<std::size_t>(count, end - cur);
+        std::copy(cur, cur + res, buffer);
+        cur += res;
+        return res;
+        
+    }
+    T peek() override {
+        
+        if(cur == end) return 0;
+        else return *cur;
+        
+    }
+    std::size_t peek(T* buffer, std::size_t count) override {
+        
+        std::size_t res = std::min<std::size_t>(count, end - cur);
+        std::copy(cur, cur + res, buffer);
+        return res;
+        
+    }
+    
+    bool isWriteable() const override { return true; }
+    void write(T value) override {
+        
+        if(cur == end) return;
+        else *(cur++) = value;
+        
+    }
+    void write(const T* buffer, std::size_t count) override {
+        
+        std::size_t res = std::min<std::size_t>(count, end - cur);
+        std::copy(buffer, buffer + res, cur);
+        cur += res;
+        
+    }
+    void flush() override {}
+    
+    bool isSeekable() const override { return true; }
+    using typename Stream<T>::SeekOrigin;
+    std::intmax_t seek(std::intmax_t offset, SeekOrigin origin) override {
+        
+        std::intmax_t pos;
+        switch(origin) {
+        
+        case SeekOrigin::Begin: pos = offset; break;
+        case SeekOrigin::Current: pos = cur - data + offset; break;
+        case SeekOrigin::End: pos = end - data + offset; break;
+        default: throw std::runtime_error("invalid origin");
+        
+        }
+        pos = std::min<std::intmax_t>(std::max<std::intmax_t>(pos, 0), end - data);
+        cur = data + pos;
+        return pos;
+        
+    }
+    std::intmax_t getPosition() override { return cur - data; }
+    
+};
+
+template <typename T>
+inline MemoryStream<T> createMemoryStream(T* buffer, std::size_t size) { return MemoryStream<T>(buffer, size); }
+
+
 template <typename T, typename = void>
 class WrapperStream;
 template <>
@@ -216,18 +304,18 @@ public:
     
     bool isReadable() const override { return true; };
     using Stream<>::read;
-    std::size_t read(char* data, std::size_t count) override {
+    std::size_t read(char* buffer, std::size_t count) override {
         
-        std::size_t ret = std::fread(data, 1, count, file);
+        std::size_t ret = std::fread(buffer, 1, count, file);
         return ret;
         
     }
     
     bool isWriteable() const override { return true; };
     using Stream<>::write;
-    void write(const char* data, std::size_t count) override {
+    void write(const char* buffer, std::size_t count) override {
         
-        std::fwrite(data, 1, count, file);
+        std::fwrite(buffer, 1, count, file);
         
     }
     void flush() override { std::fflush(file); };
@@ -269,7 +357,7 @@ public:
     
     bool isReadable() const override { return true; };
     using Stream<>::read;
-    std::size_t read(char* data, std::size_t count) override { is->read(data, count); return is->gcount(); }
+    std::size_t read(char* buffer, std::size_t count) override { is->read(buffer, count); return is->gcount(); }
     
     bool isSeekable() const override { return true; };
     std::intmax_t seek(std::intmax_t offset, SeekOrigin origin) override {
@@ -304,7 +392,7 @@ public:
     
     bool isWriteable() const override { return true; };
     using Stream<>::write;
-    void write(const char* data, std::size_t count) override { os->write(data, count); }
+    void write(const char* buffer, std::size_t count) override { os->write(buffer, count); }
     void flush() override { os->flush(); };
     
     bool isSeekable() const override { return true; };
@@ -340,11 +428,11 @@ public:
     
     bool isReadable() const override { return true; };
     using Stream<>::read;
-    std::size_t read(char* data, std::size_t count) override { ios->read(data, count); return ios->gcount(); }
+    std::size_t read(char* buffer, std::size_t count) override { ios->read(buffer, count); return ios->gcount(); }
     
     bool isWriteable() const override { return true; };
     using Stream<>::write;
-    void write(const char* data, std::size_t count) override { ios->write(data, count); }
+    void write(const char* buffer, std::size_t count) override { ios->write(buffer, count); }
     void flush() override { ios->flush(); };
     
     bool isSeekable() const override { return true; };
