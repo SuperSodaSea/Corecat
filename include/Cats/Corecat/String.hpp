e52876fc65826ec1ee65ce09871411ead76e82dc
SuperSodaSea <bobby825@126.com>
2017-08-15 23:23:29 +0800

    Add toString

--- include/Cats/Corecat/String.hpp
+++ include/Cats/Corecat/String.hpp
@@ -425,6 +425,156 @@ using StringView8 = StringView<Charset::UTF8Charset<>>;
 using StringView16 = StringView<Charset::UTF16Charset<>>;
 using StringView32 = StringView<Charset::UTF32Charset<>>;
 
+
+namespace Impl {
+
+using namespace Cats::Corecat::Sequence;
+
+template <typename T>
+struct DecimalDigit {
+    
+    static constexpr T get(int x) { return '0' + (x % 2 ? (x / 2) % 10 : (x / 2) / 10); }
+    
+};
+template <typename T>
+using DecimalDigitTable = SequenceTable<MapperSequence<DecimalDigit<T>, IndexSequence<int, 0, 199>>>;
+
+template <typename T>
+inline void toStringMiddle2(std::uint8_t x, T*& p) {
+    
+    *reinterpret_cast<std::array<T, 2>*>(p) = reinterpret_cast<const std::array<T, 2>*>(DecimalDigitTable<T>::TABLE)[x], p += 2;
+    
+}
+template <typename T>
+inline void toStringBegin2(std::uint8_t x, T*& p) {
+    
+    if(x < 10) *p++ = '0' + x;
+    else toStringMiddle2(x, p);
+    
+}
+template <typename T>
+inline void toStringMiddle4(std::uint16_t x, T*& p) {
+    
+    std::uint8_t a = x / 100, b = x % 100;
+    toStringMiddle2(a, p); toStringMiddle2(b, p);
+    
+}
+template <typename T>
+inline void toStringBegin4(std::uint16_t x, T*& p) {
+    
+    if(x < 100) toStringBegin2(x, p);
+    else { std::uint8_t a = x / 100, b = x % 100; toStringBegin2(a, p), toStringMiddle2(b, p); }
+    
+}
+template <typename T>
+inline void toStringMiddle8(std::uint32_t x, T*& p) {
+    
+    std::uint16_t a = x / 10000, b = x % 10000;
+    toStringMiddle4(a, p), toStringMiddle4(b, p);
+    
+}
+template <typename T>
+inline void toStringBegin8(std::uint32_t x, T*& p) {
+    
+    if(x < 10000) toStringBegin4(x, p);
+    else { std::uint16_t a = x / 10000, b = x % 10000; toStringBegin4(a, p), toStringMiddle4(b, p); }
+    
+}
+template <typename T>
+inline void toStringMiddle16(std::uint64_t x, T*& p) {
+    
+    std::uint32_t a = x / 100000000, b = x % 100000000;
+    toStringMiddle8(a, p), toStringMiddle8(b, p);
+    
+}
+
+}
+
+template <typename T>
+inline T* u32ToString(std::uint32_t x, T* p) {
+    
+    if(x < 100000000) Impl::toStringBegin8(x, p);
+    else {
+        std::uint32_t a = x / 100000000, b = x % 100000000;
+        Impl::toStringBegin2(a, p), Impl::toStringMiddle8(b, p);
+    }
+    return p;
+    
+}
+template <typename T>
+inline T* i32ToString(std::int32_t x, T* p) {
+    
+    std::uint32_t t;
+    if(x >= 0) t = x;
+    else *p++ = '-', t = -std::uint32_t(x);
+    return u32ToString(t, p);
+    
+}
+template <typename T>
+inline T* u64ToString(std::uint64_t x, T* p) {
+    
+    if(x < 100000000) Impl::toStringBegin8(x, p);
+    else if(x < 10000000000000000) {
+        std::uint32_t a = x / 100000000, b = x % 100000000;
+        Impl::toStringBegin8(a, p), Impl::toStringMiddle8(b, p);
+    } else {
+        std::uint64_t a = x / 10000000000000000, b = x % 10000000000000000;
+        Impl::toStringBegin4(a, p), Impl::toStringMiddle16(b, p);
+    }
+    return p;
+    
+}
+template <typename T>
+inline T* i64ToString(std::int64_t x, T* p) {
+    
+    std::uint64_t t;
+    if(x >= 0) t = x;
+    else *p++ = '-', t = -std::uint64_t(x);
+    return u64ToString(t, p);
+    
+}
+
+
+template <typename C, typename T>
+String<C> toString(T t, typename std::enable_if<std::is_unsigned<T>::value && sizeof(T) <= 4>::type* = 0) {
+    
+    String<C> s;
+    s.setLength(u32ToString(t, s.getData()) - s.getData());
+    return s;
+    
+}
+template <typename C, typename T>
+String<C> toString(T t, typename std::enable_if<std::is_signed<T>::value && sizeof(T) <= 4>::type* = 0) {
+    
+    String<C> s;
+    s.setLength(i32ToString(t, s.getData()) - s.getData());
+    return s;
+    
+}
+template <typename C, typename T>
+String<C> toString(T t, typename std::enable_if<(std::is_unsigned<T>::value && sizeof(T) > 4 && sizeof(T) <= 8)>::type* = 0) {
+    
+    String<C> s;
+    s.setLength(u64ToString(t, s.getData()) - s.getData());
+    return s;
+    
+}
+template <typename C, typename T>
+String<C> toString(T t, typename std::enable_if<(std::is_signed<T>::value && sizeof(T) > 4 && sizeof(T) <= 8)>::type* = 0) {
+    
+    String<C> s;
+    s.setLength(i64ToString(t, s.getData()) - s.getData());
+    return s;
+    
+}
+
+template <typename T>
+String8 toString8(T t) { return toString<Charset::UTF8Charset<>>(t); }
+template <typename T>
+String16 toString16(T t) { return toString<Charset::UTF16Charset<>>(t); }
+template <typename T>
+String32 toString32(T t) { return toString<Charset::UTF32Charset<>>(t); }
+
 }
 }
 
