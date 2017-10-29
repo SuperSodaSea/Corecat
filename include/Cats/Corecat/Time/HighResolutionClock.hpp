029bfd7be99753f6da95f3fefa160a79dac5e820
SuperSodaSea <bobby825@126.com>
2017-10-29 22:04:05 +0800

    Update HighResolutionClock

--- include/Cats/Corecat/Time/HighResolutionClock.hpp
+++ include/Cats/Corecat/Time/HighResolutionClock.hpp
@@ -42,7 +42,7 @@ struct HighResolutionClock {
     using rep = double;
     using period = std::ratio<1>;
     using duration = std::chrono::duration<rep, period>;
-    using time_point = std::chrono::time_point<BenchmarkClock>;
+    using time_point = std::chrono::time_point<HighResolutionClock>;
     
     static constexpr bool is_steady = false;
     
