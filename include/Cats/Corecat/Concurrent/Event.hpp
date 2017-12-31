dc6c6c9263cc06b15a4aa1b9e66a7a1dd5dd44e0
SuperSodaSea <bobby825@126.com>
2018-01-01 00:04:00 +0800

    Update license

--- include/Cats/Corecat/Concurrent/Event.hpp
+++ include/Cats/Corecat/Concurrent/Event.hpp
@@ -2,7 +2,7 @@
  *
  * MIT License
  *
- * Copyright (c) 2016-2017 The Cats Project
+ * Copyright (c) 2016-2018 The Cats Project
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
