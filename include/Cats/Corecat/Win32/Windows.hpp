e6d56d62eb7616bb2c4ae95371430cd408c75bf6
SuperSodaSea <bobby825@126.com>
2018-02-15 15:04:25 +0800

    Update Windows.hpp

--- include/Cats/Corecat/Win32/Windows.hpp
+++ include/Cats/Corecat/Win32/Windows.hpp
@@ -29,11 +29,15 @@
 
 
 #ifndef _WIN32_WINNT
-#define _WIN32_WINNT 0x0501
+#   define _WIN32_WINNT 0x0501
 #endif
 
 #ifndef NOMINMAX
-#define NOMINMAX
+#   define NOMINMAX
+#endif
+
+#ifndef WIN32_LEAN_AND_MEAN
+#   define WIN32_LEAN_AND_MEAN
 #endif
 
 #include <windows.h>
