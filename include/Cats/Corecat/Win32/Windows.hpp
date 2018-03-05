71d4a60c3d1af3d1a76e670b6213e9b178056a39
SuperSodaSea <bobby825@126.com>
2018-03-05 17:39:06 +0800

    Update Windows.h

--- include/Cats/Corecat/Win32/Windows.hpp
+++ include/Cats/Corecat/Win32/Windows.hpp
@@ -29,7 +29,10 @@
 
 
 #ifndef _WIN32_WINNT
-#   define _WIN32_WINNT 0x0501
+#   define _WIN32_WINNT 0x0600
+#elif _WIN32_WINNT < 0x0600
+#   undef _WIN32_WINNT
+#   define _WIN32_WINNT 0x0600
 #endif
 
 #ifndef NOMINMAX
