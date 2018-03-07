ec16688977cfed517e5d10715fe52baa32cfea46
SuperSodaSea <bobby825@126.com>
2018-03-08 00:15:11 +0800

    Add Corecat.hpp

---
+++ include/Cats/Corecat.hpp
@@ -0,0 +1,41 @@
+/*
+ *
+ * MIT License
+ *
+ * Copyright (c) 2016-2018 The Cats Project
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+#ifndef CATS_CORECAT_HPP
+#define CATS_CORECAT_HPP
+
+
+#include "Corecat/Concurrent.hpp"
+#include "Corecat/Data.hpp"
+#include "Corecat/System.hpp"
+#include "Corecat/Text.hpp"
+#include "Corecat/Time.hpp"
+#include "Corecat/Util.hpp"
+#include "Corecat/Win32.hpp"
+#include "Corecat/X86.hpp"
+
+
+#endif
