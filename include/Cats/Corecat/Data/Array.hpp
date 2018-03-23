8ce458fc9a1e9b1dbcb930566adc9bcac4fa5738
SuperSodaSea <bobby825@126.com>
2018-03-23 22:01:57 +0800

    Update Array

--- include/Cats/Corecat/Data/Array.hpp
+++ include/Cats/Corecat/Data/Array.hpp
@@ -42,6 +42,75 @@ namespace Corecat {
 inline namespace Data {
 
 template <typename T>
+class ArrayView;
+
+template <typename T, typename A = DefaultAllocator>
+class Array {
+    
+public:
+    
+    using Type = T;
+    using AllocatorType = A;
+    
+    using Iterator = Type*;
+    using ConstIterator = const Type*;
+    using ReverseIterator = Corecat::ReverseIterator<Iterator>;
+    using ConstReverseIterator = Corecat::ReverseIterator<ConstIterator>;
+    
+private:
+    
+    A allocator;
+    Type* data = nullptr;
+    std::size_t size = 0;
+    std::size_t capacity = nullptr;
+    
+public:
+    
+    Array() = default;
+    Array(std::size_t size_) noexcept { resize(size_); }
+    Array(const Array& src) = delete;
+    ~Array() {
+        
+        clear();
+        if(data) allocator.deallocate(data, capacity * sizeof(T));
+        
+    }
+    
+    Array& operator =(const Array& src) = delete;
+    
+    operator ArrayView<const Type>() const noexcept { return {data, size}; }
+    operator ArrayView<Type>() noexcept { return {data, size}; }
+    
+    const Type& operator [](std::size_t index) const noexcept { return data[index]; }
+    Type& operator [](std::size_t index) noexcept { return data[index]; }
+    
+    const Type* getData() const noexcept { return data; }
+    Type* getData() noexcept { return data; }
+    std::size_t getSize() const noexcept { return size; }
+    
+    bool isEmpty() const noexcept { return !size; }
+    
+    void clear() noexcept { resize(0); }
+    
+    void resize(std::size_t size_) {
+        
+    }
+    
+    Iterator begin() const noexcept { return data; }
+    Iterator end() const noexcept { return data + size; }
+    
+    ConstIterator cbegin() const noexcept { return begin(); }
+    ConstIterator cend() const noexcept { return end(); }
+    
+    ReverseIterator rbegin() const noexcept { return ReverseIterator(end()); }
+    ReverseIterator rend() const noexcept { return ReverseIterator(begin()); }
+    
+    ConstReverseIterator crbegin() const noexcept { return ConstReverseIterator(cend()); }
+    ConstReverseIterator crend() const noexcept { return ConstReverseIterator(cbegin()); }
+    
+};
+
+template <typename T>
 class ArrayView {
     
 public:
