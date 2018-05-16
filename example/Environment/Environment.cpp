263e35715ae16ffd9a9b331c3b6e45c3dd52f13e
SuperSodaSea <bobby825@126.com>
2018-05-17 06:52:12 +0800

    Update example

---
+++ example/Environment/Environment.cpp
@@ -0,0 +1,49 @@
+/*
+ *
+ * MIT License
+ *
+ * Copyright (c) 2016-2018 The Cats Project
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+#include <iostream>
+
+#include "Cats/Corecat/System/Environment.hpp"
+
+
+using namespace Cats::Corecat;
+
+int main(int argc, char** argv) {
+    
+    std::cout << "Argument:" << std::endl << "   ";
+    for(int i = 0; i < argc; ++i) std::cout << " " << argv[i];
+    std::cout << std::endl;
+    
+    std::cout << "Environment:" << std::endl;
+    for(auto&& x : Environment::getEnvironment())
+        std::cout << "    " << x << std::endl;
+    
+    std::cout << "Directory:" << std::endl;
+    std::cout << "    " << Environment::getCurrentDirectory() << std::endl;
+    
+    return 0;
+    
+}
